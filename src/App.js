9c9e29a057c5ec2dac53d50bbc8acd55efc51241
Ahmet Fatih Tülüce <ahmetfatihtuluce@gmail.com>
2023-08-15 20:12:19 +0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import TaskCreate from './Components/TaskCreate'
+import TaskList from './Components/TaskList'
 
 function App() {
+
+  const [tasks, setTasks] = useState([])
+  
+
+  const createTask = (title, taskDesc) => {
+    console.log(title, taskDesc);
+    const createdTasks= [
+      ...tasks, {
+        id: Math.round(Math.random()*999999),
+        title:title,
+        taskDesc: taskDesc,
+      }
+    ];
+
+    setTasks(createdTasks);
+    
+}
+  const deleteTaskById = (id) => {
+     const afterDelete = tasks.filter((task) => {
+        return task.id !== id;
+      })
+      setTasks(afterDelete);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <TaskCreate onCreate={createTask}/>
+        <h1>Tasks</h1>
+        <TaskList tasks={tasks} onDelete={deleteTaskById}/>
     </div>
   );
 }
---
+++ src/Components/TaskShow.jsx
@@ -0,0 +1,27 @@
+
+
+function TaskShow({task, onDelete}) {
+
+    const handleDeleteClick = () => {
+        onDelete(task.id)
+    }
+
+    
+    return ( 
+    <div className="task-show">
+
+        <h3 className="task-title">Available Task</h3>
+        <p>{task.title}</p>
+        <h3 className="task-title">To Do</h3>
+        <p>{task.taskDesc}</p>
+        <div>
+            <button className="task-delete" onClick={handleDeleteClick}>Delete</button>
+            <button className="task-update">Update</button>
+        </div>
+
+    </div> 
+    
+    );
+}
+
+export default TaskShow;
